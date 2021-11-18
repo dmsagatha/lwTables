b7c7c8bad1e414f459f7c1702ef002db0ddfd854
Doris Morales <dmorales55@gmail.com>
2021-11-18 09:14:00 -0500

    Dependencias y Tailwind Css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,56 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
-    mode: 'jit',
-    purge: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+  mode: 'jit',
+  purge: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './vendor/laravel/jetstream/**/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+      display: ['Poppins', 'sans-serif'],
+      body: ['Poppins', 'sans-serif']
     },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+        amber: colors.amber,
+        black: colors.black,
+        blue: colors.blue,
+        cyan: colors.cyan,
+        emerald: colors.emerald,
+        fuchsia: colors.fuchsia,
+        green: colors.green,
+        indigo: colors.indigo,
+        lime: colors.lime,
+        orange: colors.orange,
+        pink: colors.pink,
+        red: colors.red,
+        rose: colors.rose,
+        sky: colors.sky,
+        teal: colors.teal,
+        white: colors.white,
+      }
+    },
+  },
+
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+      display: ['group-hover'],
+      visibility: ['group-hover']
+    },
+  },
+
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
+  ],
 };
